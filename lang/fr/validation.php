605d8d2aeb85019d24f374343b6de03a2b768da0
AlphaDesnoc <alphadesnoc@gmail.com>
2026-05-25 15:55:31 +0200

    new project

---
+++ lang/fr/validation.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'uploaded' => 'Le fichier n\'a pas pu être envoyé. Vérifiez sa taille (max 5 Mo pour les images) ou réessayez.',
+    'image' => 'Le champ :attribute doit être une image.',
+    'mimes' => 'Le champ :attribute doit être un fichier de type : :values.',
+    'max' => [
+        'file' => 'Le fichier :attribute ne doit pas dépasser :max kilo-octets.',
+    ],
+];
